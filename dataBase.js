43aa8db91f1ca16c447376226c161b9cb74e9c34
David Contino <continodavidnicolas@gmail.com>
2024-12-02 11:44:49 +1100

    movie movie brodi

---
+++ dataBase.js
@@ -0,0 +1,25 @@
+const dataBase = {
+    me: {
+      name : 'Me before You',
+      price : 10,
+      seats : [false, false, false, false, null, null, false, null, false, false, false, false, false, false, false, false, false, false, false, false,false, false, null, null, null, false, false, false, false, false, false, false, false, false, false, false, false, false, false, null, null, false, false, false, false, false, false, false ]
+    },
+    ana: {
+      name : 'Anaabilla',
+      price : 12,
+      seats : [false, null, null, false, null, false, false, null, false, false, false, false, false, false, false, false, false, false, false, false, null, null, false, false, false, false, false, false, false, false, false, false, false, false, false, null, null, false, false, null, null, false, false, false, false, false, null, null ]
+    },
+    jo: {
+      name : 'Joker',
+      price : 8,
+      seats : [false, false, false, null, null, null, null, false, false, false, false, null, null, null, false, false, false, false, false, false, false, false, false, false, false, false, false, null, null, null, null, false, false, false, false, false, false, false, false, false, false, false, false, null, null, false, false, false ]
+    },
+    spi: {
+      name : 'Spider Man',
+      price : 9,
+      seats : [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, null, null, null, null, false, false, false, false, false, false, false, false, false, false, false, false, null, null, null, null, null, null ]
+    }
+  }
+
+
+export const dataBaseJSON = JSON.stringify(dataBase)
---
+++ main.js
@@ -0,0 +1,78 @@
+import { dataBaseJSON } from "./dataBase.js" // Simulate that bring the data from server in JSON
+
+const dataBase = JSON.parse(dataBaseJSON)
+const seats = document.querySelectorAll('.half-circle')
+const movie = document.getElementById('movie')
+const howManyTicket = document.getElementById('howManyTicket')
+const howMuchTicket = document.getElementById('howMuchTicket')
+let currentMovie = 'me'
+let currentPrice = 10
+let numberTickets = 0
+
+
+seats.forEach((seat, index) => {    // paint the seats according to their state 
+  changeSeatColor(seat, index)
+})
+
+movie.addEventListener('change', () => {    // listen when client select new movie
+  theCurrentMovie.value = movie.value
+})
+
+const theCurrentMovie = {   // when new movie was selected, this order to update the current movie data (name, price, seats state)
+  get value() {
+    return currentMovie;
+  },
+  set value(newValue) {
+    resetMovieData(newValue)
+
+    seats.forEach((seat, index) => {
+      changeSeatColor(seat, index)
+    })   
+  }
+}
+
+seats.forEach((seat, index) => {  // when client click a seat that will be selected or available
+    seat.addEventListener('click', () => {
+      const dataBaseSeat = dataBase[currentMovie].seats
+      if (dataBaseSeat[index] === false) {
+        dataBaseSeat[index] = true //from Available to Occupied
+        numberTickets++
+        updatePaymentInfo()
+        paintSeat(seat, '#dada3c')
+      } else if (dataBaseSeat[index] === true) {
+        dataBaseSeat[index] = false //from Occupied to Available
+        numberTickets--
+        updatePaymentInfo()
+        paintSeat(seat, '#2d2d3b')
+      }
+    })
+})
+
+
+
+
+
+// Functions to make the code cleaner and easier to read
+
+function resetMovieData(newValue) {
+  currentMovie = newValue;
+  numberTickets = 0
+  howManyTicket.innerHTML = 0
+  howMuchTicket.innerHTML = 0
+  currentPrice = dataBase[currentMovie].price
+}
+
+function changeSeatColor(seat, index) {
+  const state = dataBase[currentMovie].seats
+  if (state[index] === null) paintSeat(seat, 'white')
+  if (state[index] === false) paintSeat(seat, '#2d2d3b')
+}
+
+function paintSeat(seat, color){
+  seat.style.backgroundColor = color
+}
+
+function updatePaymentInfo(){
+          howManyTicket.innerHTML = numberTickets
+        howMuchTicket.innerHTML = numberTickets * currentPrice
+}
