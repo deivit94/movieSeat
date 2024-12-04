6993c42d93cb1e5e6addf4c073c2270309d6bfd2
David Contino <continodavidnicolas@gmail.com>
2024-12-04 20:57:17 +1100

    name of the array seats elements changed

--- dataBase.js
+++ dataBase.js
@@ -2,22 +2,22 @@ const dataBase = {
     me: {
       name : 'Me before You',
       price : 10,
-      seats : [false, false, false, false, null, null, false, null, false, false, false, false, false, false, false, false, false, false, false, false,false, false, null, null, null, false, false, false, false, false, false, false, false, false, false, false, false, false, false, null, null, false, false, false, false, false, false, false ]
+      seats : ['available', 'available', 'available', 'available', 'occupied', 'occupied', 'available', 'occupied', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available','available', 'available', 'occupied', 'occupied', 'occupied', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'occupied', 'occupied', 'available', 'available', 'available', 'available', 'available', 'available', 'occupied']
     },
     ana: {
       name : 'Anaabilla',
       price : 12,
-      seats : [false, null, null, false, null, false, false, null, false, false, false, false, false, false, false, false, false, false, false, false, null, null, false, false, false, false, false, false, false, false, false, false, false, false, false, null, null, false, false, null, null, false, false, false, false, false, null, null ]
+      seats : ['available', 'occupied', 'occupied', 'available', 'occupied', 'available', 'available', 'occupied', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'occupied', 'occupied', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'occupied', 'occupied', 'available', 'available', 'occupied', 'occupied', 'available', 'available', 'available', 'available', 'available', 'occupied', 'occupied' ]
     },
     jo: {
       name : 'Joker',
       price : 8,
-      seats : [false, false, false, null, null, null, null, false, false, false, false, null, null, null, false, false, false, false, false, false, false, false, false, false, false, false, false, null, null, null, null, false, false, false, false, false, false, false, false, false, false, false, false, null, null, false, false, false ]
+      seats : ['available', 'available', 'available', 'occupied', 'occupied', 'occupied', 'occupied', 'available', 'available', 'available', 'available', 'occupied', 'occupied', 'occupied', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'occupied', 'occupied', 'occupied', 'occupied', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'occupied', 'occupied', 'available', 'available', 'available' ]
     },
     spi: {
       name : 'Spider Man',
       price : 9,
-      seats : [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, null, null, null, null, false, false, false, false, false, false, false, false, false, false, false, false, null, null, null, null, null, null ]
+      seats : ['available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'occupied', 'occupied', 'occupied', 'occupied', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'available', 'occupied', 'occupied', 'occupied', 'occupied', 'occupied', 'occupied' ]
     }
   }
 
